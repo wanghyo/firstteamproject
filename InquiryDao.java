b953ad02c40daf67626c72d674604c29ec92f2e5
wanghyo <141088480+wanghyo@users.noreply.github.com>
2023-11-22 01:41:00 +0900

    third - correction 1

--- InquiryDao.java
+++ InquiryDao.java
@@ -9,9 +9,9 @@ import com.soolwhale.client.inquiry.vo.InquiryVO;
 
 @Mapper
 public interface InquiryDao {
-	public List<InquiryVO> inquiryList(InquiryVO ivo);
+	public List<InquiryVO> inquiryList(InquiryVO ivo); 
 	public int inquiryInsert(InquiryVO ivo);
-	public InquiryVO inquiryDetail(InquiryVO ivo); //update 폼 겸용
+	public InquiryVO inquiryDetail(InquiryVO ivo); 
 	
 	public int inquiryUpdate(InquiryVO ivo) throws Exception;
 	
@@ -19,17 +19,7 @@ public interface InquiryDao {
 	
 	public int questionDelete(InquiryVO vo) throws Exception;
 	
-	List<InquiryVO> getListWithPaging(InquiryVO ivo);
-	
-
-
-	
-	
-	
-	public List<InquiryVO> inquiryListManagement();
-
-
-
+	List<InquiryVO> getListWithPaging(InquiryVO ivo); //페이징 처리
 
 
 }
